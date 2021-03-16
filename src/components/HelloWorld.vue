86d19d59954b9fad1cf2fa518cdbd9efa11fd859
Filip Vanden Eynde <filipve1994@gmail.com>
2021-03-16 15:01:47 +0100

    fix name of css file imported

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -116,8 +116,19 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: {
+      type: String,
+      required: false
+    }
+
+  },
+  setup(props) {
+    const printMsg = (msg: string) => console.log(`The message is: ${msg}`);
+
+    return {
+      printMsg,
+    };
+  },
 });
 </script>
 
